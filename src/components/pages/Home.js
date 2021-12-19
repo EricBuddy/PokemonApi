e46af489d3317ec27506e546f0030a57ca693649
EricBuddy <Eric.Buddy00@gmail.com>
2021-12-19 14:35:03 -0500

    finish the home page and a button link

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import './Home.css'
+import {Link } from 'react-router-dom'
 function Home() {
     return (
-        <div>
-            sdfgdf
+        <div className='home'>
+           <div className='title'>
+              <h1> Welcome to pokemon api.</h1>
+           </div>
+
+            <div className='button'>
+                <Link to ='/non-shiny'><button><span>Non Shiny Pokemons</span></button></Link>
+                <Link to ='/shiny'><button><span>Skiny Pokemons</span></button></Link>
+                </div>
+
+
+
         </div>
     )
 }
