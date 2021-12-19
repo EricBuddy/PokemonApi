f9c75b7b2f69fbbfe88192b14b39cdfb5bffdc4c
EricBuddy <Eric.Buddy00@gmail.com>
2021-12-19 15:21:44 -0500

    get the api on shiny page

--- src/components/pages/Shiny.js
+++ src/components/pages/Shiny.js
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import './Shiny.css'
+import Axios from 'axios'
 function Shiny() {
+    
+    const[shiny,setShiny]=useState('')
+
+
+    const search=()=>{
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${shiny}`) .then(
+            (res)=>{
+               console.log(res) 
+            }
+        );
+    }
+
+
     return (
-        <div>
-            asdfasdf
+        <div className='shiny'>
+            <div className='text'>
+                Please Enter A Pokemon Name<br/>
+                <input type='text' onChange={e => setShiny(e.target.value)}value={shiny} placeholder='Ex: pikachu'></input>
+                <button onClick={search}>Search</button>
+            </div>
         </div>
     )
 }
