1897e1de4b1ac8e6e7f8167a3172e25bf730d90c
EricBuddy <Eric.Buddy00@gmail.com>
2021-12-19 14:03:11 -0500

    add the home, shiny, and non shiny pages

---
+++ src/components/pages/NonShiny.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NonShiny() {
+    return (
+        <div>
+          sdfsdfsfda  
+        </div>
+    )
+}
+
+export default NonShiny
---
+++ src/components/pages/Shiny.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Shiny() {
+    return (
+        <div>
+            asdfasdf
+        </div>
+    )
+}
+
+export default Shiny
